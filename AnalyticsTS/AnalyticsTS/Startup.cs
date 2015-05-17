9eb7143bf58d9a2ba2fbbadb288cf7f98542c5ea
lobasty <lobasty@gmail.com>
2015-05-17 14:32:43 +0300

    Initial Project

---
+++ AnalyticsTS/AnalyticsTS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnalyticsTS.Startup))]
+namespace AnalyticsTS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
